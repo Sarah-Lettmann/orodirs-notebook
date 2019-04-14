44560d6522636412f294a576dba2419c4a7c9570
Sarah Lettmann <sarah@td-lettmann.de>
2019-04-14 11:58:33 +0200

    grav not used anymore

--- cache/compiled/files/1d12a8683c758b7114d77c23668fe879.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => 'C:/xampp/htdocs/grav-admin/user/config/security.yaml',
-    'modified' => 1555169952,
-    'data' => [
-        'salt' => 'isCZV7uJxISjeP'
-    ]
-];
