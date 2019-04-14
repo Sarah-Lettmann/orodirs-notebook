7e51771ed44620b5841ad47acdc023fc78bad82e
felix.klose <felix.klose@klosed-source.com>
2019-04-14 20:14:54 +0200

    Updated bootstrap file to install php7.3 and mysql

---
+++ site/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de_DE">
+  <head>
+    <title>Orodir's Notebook</title>
+  </head>
+  <body>
+
+    <?php echo('Yo!'); ?>
+    Hello world!
+
+  </body>
+</html>
--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,4 +5,19 @@ apt-get install -y apache2
 
 cp /vagrant/config/000-default.conf /etc/apache2/sites-available
 
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+apt-get -y install mysql-server
+
+LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y
+apt-get update
+apt-get -y install php7.3 php7.3-cli php7.3-common libapache2-mod-php7.3 php7.3-mbstring php7.3-xmlrpc php7.3-soap php7.3-gd php7.3-xml php7.3-zip php7.3-mysql
+a2enmod php7.3
+
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+
+a2enmod rewrite
+
+apt-get -y upgrade
+
 systemctl restart apache2
